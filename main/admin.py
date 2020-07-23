48433800f35a3bfc503d1c17a4073d694ee80cdf
Anthony <gamerseth@protonmail.com>
2020-07-22 23:28:09 -0400

    completed model? hopefully it works?

--- main/admin.py
+++ main/admin.py
@@ -5,14 +5,7 @@ from django.contrib.auth.models import User
 from main.models import *
 
 
-
-class PatientAdmin(admin.ModelAdmin):
-    # pass what attributes to display and what search field will look for in admin
-    list_display = ('status', 'user', '__str__')
-    search_fields = ('status', 'user', '__str__')
-    pass
-
-
-admin.site.register(Patient, PatientAdmin)
+admin.site.register(Patient)
+admin.site.register(CovidTest)
 admin.site.register(NewsArticle)
 admin.site.register(ScientificArticle)
--- main/models.py
+++ main/models.py
@@ -4,29 +4,152 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-class Patient(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    STATUS_CHOICES = [
-        ('NA', "NA"),
+class CovidTest(models.Model):
+    TYPE_CHOICES = [
+        ('ANTIBODY', "Antibody Testing"),
+        ('ANTIGEN', 'Antigen Testing'),
+    ]
+    RESULT_CHOICES = [
         ('UNKNOWN', 'Unknown'),
         ('NEGATIVE', 'Negative'),
         ('POSITIVE', 'Positive'),
-        ('RECOVERED', 'Recovered'),
     ]
-    status = models.CharField(
+    HOSPITALIZATION_CHOICES = [
+        ('YES', "Yes"),
+        ('NO', 'No'),
+    ]
+    VENTILATION_CHOICES = [
+        ('YES', "Yes"),
+        ('NO', 'No'),
+    ]
+    INTERVENTION_CHOICES = [
+        ('YES', "Yes"),
+        ('NO', 'No'),
+    ]
+    MEDICATION_CHOICES = [
+        ('HCQ', "Hydroxychloroquine"),
+    ]
+    Date = models.DateField()
+    type = models.CharField(
         max_length=10,
-        choices=STATUS_CHOICES,
-        default='NA',
+        choices=TYPE_CHOICES,
+        default='ANTIBODY',
+    )
+    results = models.CharField(
+        max_length=10,
+        choices=RESULT_CHOICES,
+        default='UNKNOWN',
+    )
+    hospitalization = models.CharField(
+        max_length=10,
+        choices=HOSPITALIZATION_CHOICES,
+        default='NO',
+    )
+    ventilation = models.CharField(
+        max_length=10,
+        choices=VENTILATION_CHOICES,
+        default='NO',
+    )
+    medication = models.CharField(
+        max_length=10,
+        choices=MEDICATION_CHOICES,
+        default='HCQ',
     )
 
-    TESTED_CHOICES = [
-        ('BLOOD', "Antibody Testing"),
-        ('SWAB', 'Neonatal Nasopharyngeal Swabs'),
+
+class Patient(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    covidTests = models.ForeignKey(CovidTest, on_delete=models.CASCADE)
+    SMOKING_CHOICES = [
+        ('CURRENT', "Current Smoker"),
+        ('FORMER', 'Former Smoker'),
+        ('NEVER', 'Never Smoker'),
     ]
-    tested = models.CharField(
+
+    ALCOHOL_CHOICES = [
+        ('CURRENT', "Current Smoker"),
+        ('FORMER', 'Former Smoker'),
+        ('NEGATIVE', 'Never Smoker'),
+    ]
+    HOUSING_CHOICES = [
+        ('ALONE', "Live Alone"),
+        ('GROUP', 'Live With Others'),
+    ]
+    MARTIAL_CHOICES = [
+        ('SINGLE', "Single"),
+        ('MARRIED', 'Married'),
+    ]
+    INSURANCE_CHOICES = [
+        ('UNINSURED', "UnInsured"),
+        ('INSURED', 'Insured'),
+    ]
+    EMPLOYMENT_CHOICES = [
+        ('EMPLOYED', "Employed"),
+        ('UNEMPLOYED', 'UnEmployed'),
+    ]
+    DIET_CHOICES = [
+        ('VEGAN', "Vegan"),
+        ('VEGETARIAN', 'Vegetarian'),
+        ('REGULAR', 'Regular'),
+    ]
+    VETERAN_CHOICES = [
+        ('NOT', 'Not A Veteran'),
+        ('FORMER', "Former Veteran"),
+        ('CURRENT', 'Current Veteran'),
+    ]
+    DISABILITIES_CHOICES = [
+        ('DISABLE', 'Disabled'),
+        ('ABLE', "Able"),
+    ]
+
+    age = models.IntegerField(default=69,)
+    height = models.IntegerField()
+    weight = models.IntegerField()
+
+    smoking = models.CharField(
         max_length=10,
-        choices=TESTED_CHOICES,
-        default='BLOOD',
+        choices=SMOKING_CHOICES,
+        default='NEVER',
+    )
+    drinking = models.CharField(
+        max_length=10,
+        choices=ALCOHOL_CHOICES,
+        default='NEVER',
+    )
+    housing = models.CharField(
+        max_length=10,
+        choices=HOUSING_CHOICES,
+        default='ALONE',
+    )
+    martial = models.CharField(
+        max_length=10,
+        choices=MARTIAL_CHOICES,
+        default='ALONE',
+    )
+    insurance = models.CharField(
+        max_length=10,
+        choices=INSURANCE_CHOICES,
+        default='UNINSURED',
+    )
+    employment = models.CharField(
+        max_length=10,
+        choices=EMPLOYMENT_CHOICES,
+        default='INSURED',
+    )
+    diet = models.CharField(
+        max_length=10,
+        choices=DIET_CHOICES,
+        default='REGULAR',
+    )
+    veteran = models.CharField(
+        max_length=10,
+        choices=VETERAN_CHOICES,
+        default='NOT',
+    )
+    disabilities = models.CharField(
+        max_length=10,
+        choices=DISABILITIES_CHOICES,
+        default='ABLE',
     )
 
     def __str__(self):
@@ -49,3 +172,10 @@ class NewsArticle(models.Model):
     date = models.DateField()
     author = models.CharField(max_length=100)
     website = models.CharField(max_length=100)
+
+# class ClinicalTrials(models.Model):
+#     What they are searching for'
+#
+#     location = models.geolocation()
+
+
