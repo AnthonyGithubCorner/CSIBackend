757fd380f5d5483ead1acdc5e69964cb8b3d1a92
Anthony <gamerseth@protonmail.com>
2020-07-23 15:06:07 -0400

    checking git

--- CSIBackend/settings.py
+++ CSIBackend/settings.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #for geo
     'django.contrib.gis',
     'rest_framework',
     'corsheaders',
