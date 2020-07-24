ab26a4a0e3a56744e343e4408c31114684133748
XXtechsec <tony.lazar.mi@gmail.com>
2020-07-23 21:18:18 -0400

    admin

--- main/admin.py
+++ main/admin.py
@@ -1,10 +1,8 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.models import User
-
+from django.contrib import admin
 from main.models import *
-from django.contrib.gis import admin
-
 
 class CovidTestInline(admin.TabularInline):
     model = CovidTest
@@ -14,8 +12,6 @@ class PatientAdmin(admin.ModelAdmin):
     inlines = [
         CovidTestInline,
     ]
-
-
 
 admin.site.register(Patient, PatientAdmin)
 admin.site.register(CovidTest)
