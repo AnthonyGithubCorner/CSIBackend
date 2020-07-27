4b605f6a8a8166c442976f520641952eb6a04feb
XXtechsec <tony.lazar.mi@gmail.com>
2020-07-27 11:49:03 -0400

    forced it to use geolocation

--- CSIBackend/settings.py
+++ CSIBackend/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'corsheaders',
     'django.contrib.gis',
-    'main'
+    'main',
 ]
 
 MIDDLEWARE = [
@@ -157,3 +157,4 @@ GDAL_LIBRARY_PATH = os.environ.get('GDAL_LIBRARY_PATH')
 GEOS_LIBRARY_PATH = os.environ.get('GEOS_LIBRARY_PATH')
 
 django_heroku.settings(locals())
+DATABASES['default']['ENGINE'] = 'django.contrib.gis.db.backends.postgis'
