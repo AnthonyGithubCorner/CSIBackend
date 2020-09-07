585b258af290a427203bb85e4203614dab669400
XXtechsec <tony.lazar.mi@gmail.com>
2020-09-07 18:25:24 -0400

    made authetication not included when logining

--- main/views.py
+++ main/views.py
@@ -1,7 +1,7 @@
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, authentication_classes
 from rest_framework import status
-
+from rest_framework.authentication import TokenAuthentication
 from django.http import Http404
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -45,7 +45,9 @@ class UserList(generics.ListCreateAPIView):
     serializer_class = UserSerializer
     ordering_fields = ['username', 'email', 'id']
 
+
 @api_view(['PUT', 'DELETE'])
+@authentication_classes([TokenAuthentication])
 def users_detail(request, pk):
     try:
         user = User.objects.get(pk=pk)
