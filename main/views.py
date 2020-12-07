cc35cec39481a9168239cdd5c291cdc604b00475
XXtechsec <tony.lazar.mi@gmail.com>
2020-12-06 21:58:19 -0500

    reversed error and sucess XD

--- main/views.py
+++ main/views.py
@@ -62,8 +62,9 @@ def user_create(request):
     if request.method == 'POST':
         serializer = UserSerializer(data=request.data)
         if serializer.is_valid():
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-        serializer.save()
+            serializer.save()
+            return Response(serializer.validated_data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 @api_view(['PUT', 'DELETE'])
