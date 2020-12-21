e96d5a774c85cbd57c02b1ce6aba204ae8d23b34
XXtechsec <tony.lazar.mi@gmail.com>
2020-12-21 17:03:13 -0500

    this is proving quite strange?

--- main/views.py
+++ main/views.py
@@ -64,7 +64,8 @@ def user_create(request):
         if serializer.is_valid():
             obj = serializer.save()
             serialized = UserSerializer('json', [ obj, ])
-            return Response(serialized.validated_data, status=status.HTTP_201_CREATED)
+            if serialized.is_valid():
+                return Response(serialized.validated_data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
